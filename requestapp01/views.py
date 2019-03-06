f9dc68d607600d15626e97cdcea6f2282b635d34
Buvaneshwari <buvaneshwari.devi@fincity.in>
2019-03-06 11:42:46 +0530

    insted of lots of class based views to mixins

--- requestapp01/views.py
+++ requestapp01/views.py
@@ -1,3 +1,46 @@
+
+#using mixins insted of entir class
+from requestapp01.models import Snippet
+from requestapp01.serializers import SnippetSerializer
+from rest_framework import mixins
+from rest_framework import generics
+
+class SnippetList(mixins.ListModelMixin,
+                  mixins.CreateModelMixin,
+                  generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+class SnippetDetail(mixins.RetrieveModelMixin,
+                    mixins.UpdateModelMixin,
+                    mixins.DestroyModelMixin,
+                    generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
+
+
+
+
+
+
+'''
+# class based views
 from requestapp01.models import Snippet
 from requestapp01.serializers import SnippetSerializer
 from django.http import Http404
@@ -50,7 +93,7 @@ class SnippetDetail(APIView):
         snippet = self.get_object(pk)
         snippet.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
-
+'''
 
 
 
