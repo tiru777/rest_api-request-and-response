559f786d3cad9867561d0319102d2d1100519c16
Buvaneshwari <buvaneshwari.devi@fincity.in>
2019-03-06 11:36:21 +0530

    insted of function based here class based views created

--- requestapp01/urls.py
+++ requestapp01/urls.py
@@ -3,9 +3,17 @@ from . import views
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
-    path('snippets/', views.snippet_list),
+    #function based views url
+    #path('snippets/', views.snippet_list),
     #path('snippets/<int:pk>/', views.snippet_detail),
-    path('snippets/detail/<int:pk>/', views.snippet_detail),
+    #path('snippets/detail/<int:pk>/', views.snippet_detail),
+
+    #class based views urls
+    path('snippets/', views.SnippetList.as_view()),
+    path('snippets/detail/<int:pk>/', views.SnippetDetail.as_view()),
+
+
+
 
 ]
 urlpatterns = format_suffix_patterns(urlpatterns)
